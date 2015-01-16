598f5f26d284bbca9a225b7c289eecbeb87ef33b
Pavel Batanov <pavel@batanov.me>
2015-01-16 11:47:23 +0300

    fixed getTemplate registry method

--- src/ScayTrase/SwitchableThemeBundle/Service/ThemeRegistry.php
+++ src/ScayTrase/SwitchableThemeBundle/Service/ThemeRegistry.php
@@ -25,7 +25,7 @@ class ThemeRegistry implements RegistryInterface
             return null;
         }
 
-        return $this->themes[$type]->getTemplate($layout);
+        return $this->themes[$type]->get($layout);
 
     }
 
